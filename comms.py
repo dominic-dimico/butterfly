46a70cf86fe33a9029cbe15c686770cf706a5c1b
Dominic Di'Mico <dominic.dimico@gmail.com>
2019-01-11 03:34:20 -0500

    Updated config handling

--- comms.py
+++ comms.py
@@ -6,6 +6,7 @@ import parsedatetime
 import smtplib
 import configparser
 
+
 from googlevoice import Voice
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
@@ -15,39 +16,14 @@ from email import Encoders
 
 ed  = os.environ.get('EDITOR', 'vim')
 cal = parsedatetime.Calendar();
-textconfig = configparser.ConfigParser();
-textconfig.read('text.cfg');
-mailconfig = configparser.ConfigParser();
-mailconfig.read('mail.cfg');
-
-
-
-def vim(note):
-    if not note: note = ""
-    tf = tempfile.NamedTemporaryFile(suffix=".tmp", delete=False);
-    tf.write(note)
-    tf.flush()
-    name = tf.name
-    tf.close()
-    tf = open(name, 'rw')
-    subprocess.call([ed, tf.name])
-    tf.flush()
-    note = tf.read()
-    os.unlink(tf.name)
-    return note
-
-
-
-def str2dt(string):
-      parsed = cal.parse(string)[0]
-      dt = time.strftime('%Y-%m-%d %H:%M:%S', parsed)
-      return dt;
+config = configparser.ConfigParser();
+config.read('/home/dominic/src/butterfly/butterfly.cfg');
 
 
 
 def email(address, subject, body, attach):
-    gmail_user = mailconfig['main']['email']
-    gmail_password = mailconfig['main']['password']
+    gmail_user = config['mail']['email']
+    gmail_password = config['mail']['password']
     from_addr  = gmail_user
     msg = MIMEMultipart();
     msg['From'] = gmail_user;
@@ -73,8 +49,8 @@ def email(address, subject, body, attach):
 def text(number, msg):
     voice = Voice();
     voice.login(
-                textconfig['main']['username'], 
-                textconfig['main']['password']
+                config['text']['username'], 
+                config['text']['password']
     );
     voice.send_sms(number, msg);
 
